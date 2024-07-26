bdaff4645f99ddf55b508fbc18b6baa1dd89703d
suyeon3763 <suyeon942002@naver.com>
2024-07-26 16:12:48 +0900

    나수연탐구생활.py

--- pages/나수연탐구생활.py
+++ pages/나수연탐구생활.py
@@ -3,7 +3,6 @@ import streamlit as st
 # Function to reset the quiz
 def reset_quiz():
     st.session_state.step = 1
-    st.session_state.score = 0
     st.session_state.name_correct = False
     st.session_state.food_correct = False
     st.session_state.subject_correct = False
@@ -34,10 +33,10 @@ st.title('나는 어떤 사람일까요?')
 if st.session_state.step == 1:
     st.write(questions[1]['question'])
     answer = st.text_input("정답을 입력하세요:", key='name')
-    if st.button("맞지?"):
+    if st.button("맞지?", key='submit1'):
         if answer.strip().lower() == questions[1]['answer'].lower():
             st.session_state.name_correct = True
-            st.session_state.step == 2
+            st.session_state.step = 2
         else:
             st.warning("정답이 아닙니다. 다시 시도하세요.")
             reset_quiz()
@@ -45,10 +44,10 @@ if st.session_state.step == 1:
 elif st.session_state.step == 2:
     st.write(questions[2]['question'])
     answer = st.text_input("정답을 입력하세요:", key='food')
-    if st.button("맞지?"):
+    if st.button("맞지?", key='submit2'):
         if answer.strip().lower() == questions[2]['answer'].lower():
             st.session_state.food_correct = True
-            st.session_state.step == 3
+            st.session_state.step = 3
         else:
             st.warning("정답이 아닙니다. 다시 시도하세요.")
             reset_quiz()
@@ -56,7 +55,7 @@ elif st.session_state.step == 2:
 elif st.session_state.step == 3:
     st.write(questions[3]['question'])
     answer = st.text_input("정답을 입력하세요:", key='subject')
-    if st.button("맞지?"):
+    if st.button("맞지?", key='submit3'):
         if answer.strip().lower() == questions[3]['answer'].lower():
             st.session_state.subject_correct = True
             st.success("축하합니다! 모든 문제를 맞혔습니다.")
